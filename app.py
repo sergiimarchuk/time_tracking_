46b163c2fcfbd081699191714cf366538dc2de1d
sergii.marchuk <sergii.marchuk@gmail.com>
2025-07-13 11:35:18 +0200

    updated some code ref to pictures

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def work_hours(year, month, day):
     form = WorkHoursForm()
 
     if form.validate_on_submit():
-        # here can doing anythong to insert some data into db 
+        # here can doing Something to insert some data into db 
         # example:
         # save_task(session['uid'], date_obj, form.task_name.data, form.start_time.data, form.end_time.data, form.extra_info.data)
 
@@ -94,6 +94,90 @@ def work_hours(year, month, day):
         return redirect(url_for('work_hours', year=year, month=month, day=day))
 
     return render_template('work_hours.html', date=date_obj, uid=session['uid'], form=form)
+
+@app.route('/calendar')
+def calendar():
+    if 'uid' not in session:
+        flash("Please log in.")
+        return redirect(url_for('login'))
+
+    username = session.get('username')
+    uid = session.get('uid')
+
+    month_name, year, calendar_weeks, month_num = get_current_month_year()
+    today = datetime.today()
+
+    return render_template(
+        'success.html',
+        user=username,
+        uid=uid,
+        month=month_name,
+        year=year,
+        month_num=month_num,
+        calendar_weeks=calendar_weeks,
+        today_year=today.year,
+        today_month=today.month,
+        today_day=today.day
+    )
+
+
+
+
+####
+from flask import request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/opt/dev-py/TimeTracking-dev/uploads'  # Make sure this directory exists
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'pdf', 'txt'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/auto_form', methods=['GET', 'POST'])
+def auto_form():
+    if 'uid' not in session:
+        flash("Please log in.")
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        oil_file = request.files.get('oil_file')
+        antifreeze_file = request.files.get('antifreeze_file')
+        extra_info = request.form.get('extra_info')
+
+        if oil_file and allowed_file(oil_file.filename):
+            filename = secure_filename(oil_file.filename)
+            oil_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        if antifreeze_file and allowed_file(antifreeze_file.filename):
+            filename = secure_filename(antifreeze_file.filename)
+            antifreeze_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # You can save `extra_info` or log it
+        print("Extra Info:", extra_info)
+
+        flash("Files uploaded successfully.")
+        return redirect(url_for('auto_form'))
+
+    username = session.get('username')
+    uid = session.get('uid')
+
+    month_name, year, calendar_weeks, month_num = get_current_month_year()
+    today = datetime.today()
+
+    return render_template(
+        'auto_form.html',
+        user=username,
+        uid=uid,
+        month=month_name,
+        year=year,
+        month_num=month_num,
+        calendar_weeks=calendar_weeks,
+        today_year=today.year,
+        today_month=today.month,
+        today_day=today.day
+    )
 
 
 @app.route('/logout')
