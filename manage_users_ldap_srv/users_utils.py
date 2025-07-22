6950a1cb233043150f1acb77405744a315b9da7d
sergii.marchuk <sergii.marchuk@gmail.com>
2025-07-22 20:47:45 +0200

    updated code ref ldap creation new user

--- manage_users_ldap_srv/users_utils.py
+++ manage_users_ldap_srv/users_utils.py
@@ -37,12 +37,18 @@ def create_ldif_files(login, name, email, password):
 def add_user_to_ldap(login, name, email, password):
     user_ldif, fix_ldif = create_ldif_files(login, name, email, password)
 
-    os.system(f"docker cp {user_ldif} openldap:/{login}.ldif")
-    os.system(f"docker exec openldap ldapadd -x -D 'cn=admin,dc=myorg,dc=local' -w admin -f /{login}.ldif")
+    cmds = [
+        f"docker cp {user_ldif} openldap:/{login}.ldif",
+        f"docker exec openldap ldapadd -x -D 'cn=admin,dc=myorg,dc=local' -w admin -f /{login}.ldif",
+        f"docker cp {fix_ldif} openldap:/{login}_fix.ldif",
+        f"docker exec openldap ldapmodify -x -D 'cn=admin,dc=myorg,dc=local' -w admin -f /{login}_fix.ldif"
+    ]
 
-    os.system(f"docker cp {fix_ldif} openldap:/{login}_fix.ldif")
-    os.system(f"docker exec openldap ldapmodify -x -D 'cn=admin,dc=myorg,dc=local' -w admin -f /{login}_fix.ldif")
-    
+    for cmd in cmds:
+        exit_code = os.system(cmd)
+        if exit_code != 0:
+            raise RuntimeError(f"Command failed: {cmd}")
+
     
     
     
